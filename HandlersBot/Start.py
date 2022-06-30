5f3ef2922cc4daf7bef6f80a62834140778caf2c
Artem <super.alshan@mail.ru>
2022-06-30 13:53:06 +0300

    Models work with commands "start"

---
+++ HandlersBot/Start.py
@@ -0,0 +1,19 @@
+from aiogram import types, Dispatcher
+from create_bot import bot
+from HandlersKeyboardBot.Keyboard import menu_markup
+from DBase.Dbase import create_database, add_default_settings, select_show_info_video
+
+
+async def start(msg: types.Message):
+    chat_id = msg.chat.id
+    full_name_user = msg.from_user.full_name
+
+    # Create Database
+    create_database()
+
+    await bot.send_message(chat_id, f'Приветствую тебя ✋, {full_name_user}!\n'
+                                    f'Хочешь отправить видео 🤔, тогда нажми на кнопку 👇', reply_markup=menu_markup())
+
+
+def register_handler_start(dp: Dispatcher):
+    dp.register_message_handler(start, commands=['start'])

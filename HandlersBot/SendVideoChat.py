8f293889bc52405dc7f92b1ac874e1a3a6078294
Artem <super.alshan@mail.ru>
2022-07-07 12:09:04 +0300

    Update SendVideoChat.py
    Now to send video in the chat you need to have chat id.

--- HandlersBot/SendVideoChat.py
+++ HandlersBot/SendVideoChat.py
@@ -1,5 +1,5 @@
 from aiogram import types, Dispatcher
-from config import CHANNEL_ID
+from DBase.Dbase import ChatIdDbase
 from aiogram.dispatcher.filters import Text
 from HandlersKeyboardBot.Keyboard import menu_markup, back_markup
 from create_bot import bot, dp
@@ -27,18 +27,27 @@ async def start_send_video(msg: types.Message):
         title = conversion_title_video(file_name)
         duration = conversion_duration_video(duration_video)
 
-        channel_id = CHANNEL_ID
+        channel_id = ChatIdDbase(data=None).select_chat_id()
 
-        if show_info_video == 'Выкл.':
-            await bot.send_video(channel_id, id_video)
-            await bot.send_message(chat_id, 'Видео успешно отправлено ✅', reply_markup=menu_markup())
+        try:
+            if channel_id:
+                if show_info_video == 'Выкл.':
+                    await bot.send_video(channel_id, id_video)
+                    await bot.send_message(chat_id, 'Видео успешно отправлено ✅', reply_markup=menu_markup())
 
-        else:
-            message = f'{title}\n' \
-                      f'Длительность видео: {duration}'
-            await bot.send_video(channel_id, id_video, caption=message)
-            await bot.send_message(chat_id, message)
-            await bot.send_message(chat_id, 'Видео успешно отправлено ✅', reply_markup=menu_markup())
+                else:
+                    message = f'{title}\n' \
+                              f'Длительность видео: {duration}'
+                    await bot.send_video(channel_id, id_video, caption=message)
+                    await bot.send_message(chat_id, message)
+                    await bot.send_message(chat_id, 'Видео успешно отправлено ✅', reply_markup=menu_markup())
+
+        except Exception as ex:
+            print(ex)
+            error = 'Я не могу отправить видео в ваш чат,так как вы не ввели id вашего чата или был введен ' \
+                    'неправльный id 😔.\n' \
+                    'Пожалуйста, сохраните полученный id в настройках бота или измените неправильный id ❗'
+            await bot.send_message(chat_id, error)
 
 
 async def back_menu(msg: types.Message):

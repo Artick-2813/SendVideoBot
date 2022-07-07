ec824fdb8b66283c23de43a000e8f8a6b23420cf
Artem <super.alshan@mail.ru>
2022-07-07 12:05:15 +0300

    Update Dbase.py
    I add new settings. It's save chai id telegramm.Accordingly fo work with setting chat was create class ChatIdDbase.

--- DBase/Dbase.py
+++ DBase/Dbase.py
@@ -2,7 +2,8 @@ import sqlite3
 
 DEFAULT_SETTINGS = [
     {
-        'Show_Info_Video': 'Выкл.'
+        'Show_Info_Video': 'Выкл.',
+        'Your_Chat_Id': 'У вас нет id чата'
     }
 ]
 
@@ -20,6 +21,7 @@ def create_database():
 
     sql = """ CREATE TABLE IF NOT EXISTS settings(
         show_info_video TEXT NOT NULL,
+        chat_id TEXT NOT NULL,
         UNIQUE ("show_info_video") ON CONFLICT IGNORE
     
     ) """
@@ -34,7 +36,8 @@ def add_default_settings():
 
     for elem in DEFAULT_SETTINGS:
         status = elem['Show_Info_Video']
-        cursor.execute(""" INSERT INTO settings VALUES(?)""", (status,))
+        chat_id = elem['Your_Chat_Id']
+        cursor.execute(""" INSERT INTO settings VALUES(?,?)""", (status, chat_id,))
 
     connection.commit()
 
@@ -71,3 +74,41 @@ def remove_show_info_video():
 
     cursor.execute(""" DELETE FROM settings WHERE show_info_video="Выкл." OR show_info_video="Вкл." """)
     connection.commit()
+
+
+class ChatIdDbase:
+    def __init__(self, data):
+        self.data = data
+
+    def update_chat_id(self):
+        try:
+            data = self.data
+            connection = connect()
+            cursor = connection.cursor()
+
+            cursor.execute(f""" UPDATE settings SET chat_id='{data}' """)
+
+            connection.commit()
+        except Exception as ex:
+            print(ex)
+
+    def select_chat_id(self):
+        try:
+            connection = connect()
+            cursor = connection.cursor()
+
+            sql = """SELECT chat_id FROM settings"""
+
+            cursor.execute(sql)
+
+            result = cursor.fetchone()
+
+            return result[0]
+        except Exception as ex:
+            print(ex)
+
+
+
+
+
+
